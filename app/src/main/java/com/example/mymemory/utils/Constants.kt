8e3074ae85e043af07cd6d21eaa0a9f10a31587b
JW-creator <jasonwan_js@hotmail.com>
2021-06-13 18:05:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_face,
+    R.drawable.ic_ac_unit,
+    R.drawable.ic_airport,
+    R.drawable.ic_alarm,
+    R.drawable.ic_android,
+    R.drawable.ic_basketball,
+    R.drawable.ic_money,
+    R.drawable.ic_sun,
+    R.drawable.ic_wallet,
+    R.drawable.ic_wifi,
+    R.drawable.ic_watch,
+    R.drawable.ic_cloudy
+)
